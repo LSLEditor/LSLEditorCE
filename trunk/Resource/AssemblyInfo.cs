75c722f3541904478b63f4a5e90bc0d18e25d3f6
thoysg <thoysg@3f4676ac-adda-40fd-8265-58d1435b1672>
2010-06-20 13:11:15 +0000

    Updating should work :)
    git-svn-id: https://lsleditor.svn.sourceforge.net/svnroot/lsleditor@25 3f4676ac-adda-40fd-8265-58d1435b1672

--- trunk/Resource/AssemblyInfo.cs
+++ trunk/Resource/AssemblyInfo.cs
@@ -69,7 +69,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.40.*")]
+[assembly: AssemblyVersion("2.41.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
