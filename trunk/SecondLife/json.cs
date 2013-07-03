13667e5a3ca934325faacd305c8cbd5fe15f04a3
Ima Mechanique <niel-archer@sourceforge.net>
2013-07-03 04:35:14 +0100

    Starting work on JSON functionality.

---
+++ trunk/SecondLife/json.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Text;
+
+namespace LSLEditor
+{
+	public partial class SecondLife
+	{
+        public class jsonObject : DictionaryBase
+        {
+
+        }
+    }
+}
