7a2e6b5ad71ec410d2fc8a807a9f18b3f7e13052
Ima Mechanique <niel-archer@sourceforge.net>
2013-07-07 04:45:28 +0100

    Tagging version at v2.53.1

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -70,7 +70,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("2.53.0.2")]
+[assembly: AssemblyVersion("2.53.1.2")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
@@ -100,4 +100,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyName("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyFileVersionAttribute("2.53.0.2")]
+[assembly: AssemblyFileVersionAttribute("2.53.1.2")]
