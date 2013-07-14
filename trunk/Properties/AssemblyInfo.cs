0c774368f99d146ca1a572f923c9acfd83a32487
Ima Mechanique <niel-archer@sourceforge.net>
2013-07-14 04:19:43 +0100

    Moving AssemblyRevisionIncrementer into repo tree.

--- trunk/Properties/AssemblyInfo.cs
+++ trunk/Properties/AssemblyInfo.cs
@@ -69,8 +69,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers
 // by using the '*' as shown below:
-
-[assembly: AssemblyVersion("2.999.999.1")]
+// We use a Major.Minor.Bugfix.Build pattern.
+[assembly: AssemblyVersion("2.999.999.2")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the
@@ -100,4 +100,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDelaySign(false)]
 //[assembly: AssemblyKeyName("")]
 [assembly: ComVisibleAttribute(false)]
-[assembly: AssemblyFileVersionAttribute("2.999.999.1")]
+[assembly: AssemblyFileVersionAttribute("2.999.999.2")]
