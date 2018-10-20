1f540cbfcaa58ee1cbbcc4cbfe331413168a29e7
jojorne <jojorne@users.noreply.github.com>
2018-10-19 23:34:00 -0300

    Fix white-space.

--- trunk/Properties/Settings.Designer.cs
+++ trunk/Properties/Settings.Designer.cs
@@ -1109,12 +1109,12 @@ namespace LSLEditor.Properties {
                   {
                     state_entry()
                     {
-                      llSay(0, ""Hello, Avatar!"");
+                    llSay(0, ""Hello, Avatar!"");
                     }
 
                     touch_start(integer total_number)
                     {
-                      llSay(0, ""Touched: "" + (string)total_number);
+                    llSay(0, ""Touched: "" + (string)total_number);
                     }
                   }
                 ")]
